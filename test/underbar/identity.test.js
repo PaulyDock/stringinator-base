789c105986b25c3dc8d91474874d8120891ca48d
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-07 15:33:31 -0700

    Add identity tests

--- test/underbar/identity.test.js
+++ test/underbar/identity.test.js
@@ -2,18 +2,27 @@ const _ = require('../../underbar');
 
 describe('identity()', () => {
   it('returns null if given null', () => {
-    // Your code goes here
+    expect(_.identity(null)).toEqual(null);
   });
 
   it('returns the number if given a number', () => {
-    // Your code goes here
+    expect(_.identity(5)).toEqual(5);
   });
 
   it('returns the same array if given an array', () => {
-    // Your code goes here
+    expect(_.identity([1, 2, 'tree'])).toEqual([1, 2, 'tree']);
   });
 
   it('returns the same object if given an object', () => {
-    // Your code goes here
+    const obj = {
+      'a':'1',
+      'b':'2',
+      'Tree':'3'
+    }
+    expect(_.identity(obj)).toEqual({
+      'a':'1',
+      'b':'2',
+      'Tree':'3'
+    });
   });
 });
