71e5e2afa10dc139265cacaef7efdabe66f2528b
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-07 16:36:12 -0700

    Add isArrayLike.test to underbar

--- test/underbar/isArrayLike.test.js
+++ test/underbar/isArrayLike.test.js
@@ -2,14 +2,23 @@ const _ = require('../../underbar');
 
 describe('isArrayLike()', () => {
   it('returns true for an actual array', () => {
-    // Your code goes here
+    expect(_.isArrayLike([1, 2, 3])).toBe(true);
   });
 
   it('returns true for an array-like object', () => {
-    // Your code goes here
+    const arrayLikeObj = {
+      'a':'air',
+      'length':1
+    }
+    expect(_.isArrayLike(arrayLikeObj)).toBe(true);
   });
 
   it('returns false for a non-array-like object', () => {
-    // Your code goes here
+    const nonArrayLikeObj = {
+      'a':'air',
+      'b':'boy',
+      'c':'cat'
+    }
+    expect(_.isArrayLike(nonArrayLikeObj)).toBe(false);
   });
 });
