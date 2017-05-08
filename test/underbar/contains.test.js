3633511e7094158a80a15ea44e06531c78a8d964
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-08 16:41:31 -0700

    Add array tests to contains in underbar

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -2,11 +2,13 @@ const _ = require('../../underbar');
 
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
-    // Your code goes here
+    let evens = [2, 4, 6, 8, 10];
+    expect(_.contains(evens, 6)).toBe(true);
   });
 
   it('returns false if an array does not contain the target', () => {
-    // Your code goes here
+    let evens = [2, 4, 6, 8, 10];
+    expect(_.contains(evens, 5)).toBe(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
