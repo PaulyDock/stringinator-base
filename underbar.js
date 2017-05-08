ab3cc5a2bf78b4b7eed517ba5be5349eea6042dc
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-08 10:07:51 -0700

    Add pluck coding to underbar

--- underbar.js
+++ underbar.js
@@ -51,10 +51,10 @@ const map = function(obj, callback=identity) {
   return results;
 };
 
-// Return an array of the values o a certain property in the collection.
+// Return an array of the values of a certain property in the collection.
 // E.g. given an array of people objects, return an array of just their ages.
 const pluck = function(obj, key) {
-  // Your code goes here
+  return map(obj, entry => entry[key]);
 };
 
 // Reduces collection to a value which is the accumulated result of running
