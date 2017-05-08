ab9b56ae0b2d7917e7fc36da7cc5eee9cdc701f0
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-08 10:08:06 -0700

    Add pluck.test to underbar

--- test/underbar/pluck.test.js
+++ test/underbar/pluck.test.js
@@ -2,7 +2,10 @@ const _ = require('../../underbar');
 
 describe('pluck()', () => {
   it('returns an array of just ages, given an array of people objects', () => {
-    // Your code goes here
+    const peopleObjs = [
+    {name: 'Tony', gender: 'male', age: 28},
+    {name: 'Alice', gender: 'female', age: 31}
+    ];
+    expect(_.pluck(peopleObjs, 'age')).toEqual([28, 31]);
   });
-
 });
