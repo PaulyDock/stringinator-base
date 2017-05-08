0df4a51b1d2852f8e3e363b0140ef3a542c47424
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-08 10:41:45 -0700

    Add reduce.test to underbar

--- test/underbar/reduce.test.js
+++ test/underbar/reduce.test.js
@@ -2,11 +2,11 @@ const _ = require('../../underbar');
 
 describe('reduce()', () => {
   it('reduces an array of numbers to a sum, *with* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    expect(_.reduce([3, 4, 5], (acc, val) => acc + val, 6)).toBe(18);
   });
 
   it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    expect(_.reduce([3, 4, 5], (acc, val) => acc + val)).toBe(12);
   });
 
 });
