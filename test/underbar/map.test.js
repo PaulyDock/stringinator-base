5e1cfe7c861c95f7cb5a56ed83de4dceda047bf9
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-08 08:45:41 -0700

    Revert "Add map coding to underbar"
    This reverts commit 34e810cc83e4702cbec627bd34a6a58648ac3d18.

--- test/underbar/map.test.js
+++ test/underbar/map.test.js
@@ -5,37 +5,3 @@ describe('map()', () => {
     // Your code goes here
   });
 });
-
-/*
-describe('each()', () => {
-  it('iterates every element of an array, passing that element, its corresponding index, and the entire array to the callback', () => {
-    expect(_.each([3, 4, 5], function(num, index, array) {
-      return [num * num, index, array];
-    })).toEqual([
-                [9, 0, [3, 4, 5]],
-                [16, 1, [3, 4, 5]],
-                [25, 2, [3, 4, 5]]
-                ]);
-  });
-
-  it('iterates every element of an array-like object, passing that element, its corresponding index, and the entire array to the callback', () => {
-    expect(_.each([3, 4, 5], function(num, index, array) {
-      return [num * num, index, array];
-    })).toEqual([
-                [9, 0, [3, 4, 5]],
-                [16, 1, [3, 4, 5]],
-                [25, 2, [3, 4, 5]]
-                ]);
-  });
-
-  it('iterates every property of an object, passing the value, the corresponding key, and the entire object to the callback', () => {
-    const res = [];
-    expect(_.each([3, 4, 5], function(num, index, array) {
-      return [num * num, index, array];
-    })).toEqual([
-                [9, 0, [3, 4, 5]],
-                [16, 1, [3, 4, 5]],
-                [25, 2, [3, 4, 5]]
-                ]);
-  });
-  */
