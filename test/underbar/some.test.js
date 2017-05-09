ca5565fd8fb623e30d4fbf43fe9a3426547a03a9
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-08 17:01:29 -0700

    Add some.test to underbar

--- test/underbar/some.test.js
+++ test/underbar/some.test.js
@@ -2,11 +2,13 @@ const _ = require('../../underbar');
 
 describe('some()', () => {
   it('returns true if any number is odd', () => {
-    // Your code goes here
+    let nums = [2, 4, 6, 8, 9];
+    expect(_.some(nums, num => num % 2 === 1)).toBe(true);
   });
 
   it('returns false if no number is odd', () => {
-    // Your code goes here
+    let evens = [2, 4, 6, 8];
+    expect(_.some(evens, num => num % 2 === 1)).toBe(false);
   });
 
 });
