a97739ff4bb2905dd9fd1e5832ce35b7b4adcdb0
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-08 18:47:34 -0700

    Add filter.test to underbar

--- test/underbar/filter.test.js
+++ test/underbar/filter.test.js
@@ -2,10 +2,19 @@ const _ = require('../../underbar');
 
 describe('filter()', () => {
   it('filters an array to odd numbers', () => {
-    // Your code goes here
+    let nums = [1, 2, 3, 4, 5, 6, 7, 8];
+    expect(_.filter(nums, num => num % 2 === 1)).toEqual([1, 3, 5, 7]);
   });
 
   it('filters an object to only numeric values', () => {
-    // Your code goes here
+    const obj = {
+      height: 54,
+      weight: 78,
+      eyeColor: 'blue',
+      hairColor: 'brown'
+    };
+    expect(_.filter(obj, value => !isNaN(value))).toEqual([54, 78]);
+    //expect(_.filter(obj, entry => typeof obj[entry] === 'number')).toEqual([54, 78]);
   });
+
 });
